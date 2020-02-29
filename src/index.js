496adb263837b578b8c70d26afdef45bab28118a
unknown <kytranl0@yahoo.com>
2020-02-29 12:52:16 -0500

    event binding

--- src/index.js
+++ src/index.js
@@ -3,20 +3,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //End
 import './index.css';
+
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {ele: 'YOYOYO'};
+        this.activateQuan = this.activateQuan.bind(this);
+    }
+
+    activateQuan() {
+        this.setState(state => ({
+            ele: state.ele + 'YOYOYO'   
+        }));
+    }
+
     render() {
         return (
+            //css ref
             <div className="game">
-                <h1>Hello</h1>
+                <h1 onClick={this.activateQuan}>{this.state.ele}</h1>
             </div>
         );
     }
 }
 
-// ========================================
 // We can reference the Game component here
 ReactDOM.render(
     <Game />,
-    // If line below is removed thn app crashes. 
     document.getElementById('root')
 );
